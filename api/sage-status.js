78d0c7a749f41839a9f0fc8cc0b4a96a0cb14588
ryantotal <0ryansodhi@gmail.com>
2025-07-27 21:12:49 +0100

    Delete api directory

--- api/sage-status.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  // Get stored access token from database
-  const token = process.env.SAGE_CLIENT_SECRET;
-  
-  res.status(200).json({ 
-    connected: !!token,
-    message: token ? 'Sage connection configured' : 'Sage not configured'
-  });
-}
