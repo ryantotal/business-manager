4fc09b794c5441e70faf01408d1533cd05bdf886
ryantotal <0ryansodhi@gmail.com>
2025-07-30 20:08:31 +0100

    Update sage-auth.js

--- api/sage-auth.js
+++ api/sage-auth.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  const authUrl = `https://www.sageone.com/oauth2/auth?client_id=${process.env.SAGE_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(process.env.SAGE_REDIRECT_URI)}&scope=full_access&state=${Math.random().toString(36).substring(7)}`;
+  const authUrl = `https://www.sageone.com/oauth2/auth?client_id=${process.env.SAGE_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent('https://portal.totalwasteservicesltd.com/api/sage-callback')}&scope=full_access&state=${Math.random().toString(36).substring(7)}`;
   res.redirect(authUrl);
 }
